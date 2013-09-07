00b04a02be11f832b6006820b1230ef68f0a62cf
Andrew Cranston <andrewtcranston@gmail.com>
2013-09-06 22:16:05 -0700

    initial rails setup files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YardBeautiful::Application.config.secret_token = '5bb2529132679ae3d2101a6e1d3231b92a27ed1d0427995631b098e35f37c05517f92b6a44fb5b786915c22980de318d4b8eeea7f1ff0e02dba5d894cc78ec4e'
